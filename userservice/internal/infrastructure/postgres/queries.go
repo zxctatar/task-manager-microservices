7fd2bac22feb6577f8983385d40f6f8a70f251c7
ashlyaud <maksimknutov.w80@gmail.com>
2026-01-17 02:47:13 +0300

    added new query

--- userservice/internal/infrastructure/postgres/queries.go
+++ userservice/internal/infrastructure/postgres/queries.go
@@ -11,4 +11,13 @@ const (
 		email 
 	FROM users 
 	WHERE email = $1`
+
+	QuerySaveUser = `
+	INSERT INTO users (
+		first_name,
+		middle_name,
+		last_name,
+		hash_password,
+		email
+	) VALUES ($1, $2, $3, $4, $5) RETURNING id`
 )
