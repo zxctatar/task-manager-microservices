e9526e12dce9779de3905321701dce719a7c2221
ashlyaud <maksimknutov.w80@gmail.com>
2026-01-18 20:59:35 +0300

    added Login implementation

--- userservice/internal/usecase/implementations/login/login.go
+++ userservice/internal/usecase/implementations/login/login.go
@@ -2,11 +2,18 @@ package login
 
 import (
 	"context"
+	"errors"
 	"log/slog"
 	"userservice/internal/repository/hasher"
+	"userservice/internal/repository/idgenerator"
 	"userservice/internal/repository/session"
 	storagerepo "userservice/internal/repository/storage"
+	logerr "userservice/internal/usecase/errors/login"
 	logmodel "userservice/internal/usecase/models/login"
+)
+
+var (
+	invalidSessionId = "invalid"
 )
 
 type LoginUC struct {
@@ -15,17 +22,58 @@ type LoginUC struct {
 	storage     storagerepo.StorageRepo
 	passHasher  hasher.PasswordHasher
 	sessionRepo session.SessionRepo
+	idgen       idgenerator.IDGenerator
 }
 
-func NewLoginUC(log *slog.Logger, storage storagerepo.StorageRepo, passHasher hasher.PasswordHasher, sessionRepo session.SessionRepo) *LoginUC {
+func NewLoginUC(
+	log *slog.Logger,
+	storage storagerepo.StorageRepo,
+	passHasher hasher.PasswordHasher,
+	sessionRepo session.SessionRepo,
+	idgen idgenerator.IDGenerator,
+) *LoginUC {
 	return &LoginUC{
 		log:         log,
 		storage:     storage,
 		passHasher:  passHasher,
 		sessionRepo: sessionRepo,
+		idgen:       idgen,
 	}
 }
 
 func (l *LoginUC) Login(ctx context.Context, in *logmodel.LoginInput) (*logmodel.LoginOutput, error) {
-	panic("not implemented")
+	const op = "login.Login"
+	log := l.log.With(slog.String("op", op), slog.String("email", in.Email))
+
+	log.Info("user login started")
+
+	ud, err := l.storage.FindByEmail(ctx, in.Email)
+	if err != nil {
+		if errors.Is(err, storagerepo.ErrNoRows) {
+			log.Info("login stopped: user not found")
+			return logmodel.NewLoginOutput(invalidSessionId), logerr.ErrUserNotFound
+		}
+		log.Warn("login stopped", slog.String("error", err.Error()))
+		return logmodel.NewLoginOutput(invalidSessionId), err
+	}
+
+	if err := l.passHasher.ComparePassword([]byte(ud.HashPassword), []byte(in.Password)); err != nil {
+		if errors.Is(err, hasher.ErrWrongPassword) {
+			log.Info("login stopped: wrong password")
+			return logmodel.NewLoginOutput(invalidSessionId), logerr.ErrInvalidPassword
+		}
+		log.Warn("login stopped", slog.String("error", err.Error()))
+		return logmodel.NewLoginOutput(invalidSessionId), err
+	}
+
+	sessionId := l.idgen.New()
+
+	if err := l.sessionRepo.Save(ctx, sessionId, ud.Id); err != nil {
+		log.Warn("login stopped: cannot save session")
+		return logmodel.NewLoginOutput(invalidSessionId), err
+	}
+
+	log.Info("user successfully login")
+
+	return logmodel.NewLoginOutput(sessionId), nil
 }
