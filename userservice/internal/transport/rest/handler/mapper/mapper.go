73fc01ef4e3479345e90f51c1c8a48ea6bc8acce
ashlyaud <maksimknutov.w80@gmail.com>
2026-01-18 22:07:04 +0300

    new methods have been added to the login handle

--- userservice/internal/transport/rest/handler/mapper/mapper.go
+++ userservice/internal/transport/rest/handler/mapper/mapper.go
@@ -1,7 +1,9 @@
 package handlmapper
 
 import (
+	logindto "userservice/internal/transport/rest/handler/dto/login"
 	regdto "userservice/internal/transport/rest/handler/dto/registration"
+	logmodel "userservice/internal/usecase/models/login"
 	regmodel "userservice/internal/usecase/models/registration"
 )
 
@@ -18,5 +20,20 @@ func RegRequestToInput(r *regdto.RegistrationRequest) *regmodel.RegInput {
 func RegOutputToResponse(ro *regmodel.RegOutput) *regdto.RegistrationResponse {
 	return &regdto.RegistrationResponse{
 		IsRegistered: ro.IsRegistered,
+	}
+}
+
+func LogRequestToInput(l *logindto.LoginRequest) *logmodel.LoginInput {
+	return logmodel.NewLoginInput(
+		l.Email,
+		l.Password,
+	)
+}
+
+func LogOutputToResponse(lo *logmodel.LoginOutput) *logindto.LoginResponse {
+	return &logindto.LoginResponse{
+		FirstName:  lo.FirstName,
+		MiddleName: lo.MiddleName,
+		LastName:   lo.LastName,
 	}
 }
