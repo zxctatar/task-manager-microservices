98226d92a9a9f2d91bf89d5bdf32c55a1b6c490f
ashlyaud <maksimknutov.w80@gmail.com>
2026-01-29 01:36:30 +0300

    chore: added function of connecting to the database

---
+++ projectservice/internal/app/postgres.go
@@ -0,0 +1,29 @@
+package app
+
+import (
+	"database/sql"
+	"fmt"
+	"projectservice/internal/config"
+)
+
+func mustLoadPostgres(cfg *config.Config) *sql.DB {
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		cfg.PostgresConf.Host,
+		cfg.PostgresConf.Port,
+		cfg.PostgresConf.User,
+		cfg.PostgresConf.Password,
+		cfg.PostgresConf.DbName,
+		cfg.PostgresConf.Sslmode,
+	)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		panic("cannot open db: " + err.Error())
+	}
+
+	if err := db.Ping(); err != nil {
+		panic("cannot ping db:" + err.Error())
+	}
+
+	return db
+}
