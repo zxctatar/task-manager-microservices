9852b44046e521d1c7da84293e46ce0abf38a6d3
ashlyaud <maksimknutov.w80@gmail.com>
2026-01-29 01:14:41 +0300

    feat: added a mapper for converting from a usecase model to a dto and vice versa

---
+++ projectservice/internal/transport/rest/handler/mapper/mapper.go
@@ -0,0 +1,16 @@
+package handlmapper
+
+import (
+	createdto "projectservice/internal/transport/rest/handler/dto/create"
+	createmodel "projectservice/internal/usecase/models/createproject"
+)
+
+func CreateRequestToInput(cr *createdto.CreateRequest) *createmodel.CreateProjectInput {
+	return createmodel.NewCreateProjectInput(cr.Name)
+}
+
+func CreateOutputToResponse(co *createmodel.CreateProjectOutput) *createdto.CreateResponse {
+	return &createdto.CreateResponse{
+		IsCreated: co.IsCreated,
+	}
+}
