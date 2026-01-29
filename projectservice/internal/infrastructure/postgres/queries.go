1dae4e3d0dcff10027f0851ef1724225fcbe515a
ashlyaud <maksimknutov.w80@gmail.com>
2026-01-30 02:24:02 +0300

    chore: querie removed

--- projectservice/internal/infrastructure/postgres/queries.go
+++ projectservice/internal/infrastructure/postgres/queries.go
@@ -1,6 +1,5 @@
 package postgres
 
 var (
-	QuerieFindByName = "SELECT id, owner_id, name, created_at FROM projects WHERE owner_id = $1 AND name = $2"
-	QuerieSave       = "INSERT INTO projects(owner_id, name) VALUES($1, $2)"
+	QuerieSave = "INSERT INTO projects(owner_id, name) VALUES($1, $2)"
 )
