33fcd97e9d03c233d3a6c0c64d3ddcc8453a7412
ashlyaud <maksimknutov.w80@gmail.com>
2026-01-31 00:56:56 +0300

    chore: added functions for mapping handler and use case structures

--- projectservice/internal/transport/rest/handler/mapper/mapper.go
+++ projectservice/internal/transport/rest/handler/mapper/mapper.go
@@ -2,7 +2,9 @@ package handlmapper
 
 import (
 	createdto "projectservice/internal/transport/rest/handler/dto/create"
+	deletedto "projectservice/internal/transport/rest/handler/dto/delete"
 	createmodel "projectservice/internal/usecase/models/createproject"
+	deletemodel "projectservice/internal/usecase/models/deleteproject"
 )
 
 func CreateRequestToInput(cr *createdto.CreateRequest, userId uint32) *createmodel.CreateProjectInput {
@@ -13,4 +15,12 @@ func CreateOutputToResponse(co *createmodel.CreateProjectOutput) *createdto.Crea
 	return &createdto.CreateResponse{
 		IsCreated: co.IsCreated,
 	}
+}
+
+func DeleteRequestToInput(dr *deletedto.DeleteRequest, userId uint32) *deletemodel.DeleteProjectInput {
+	return deletemodel.NewDeleteProjectInput(userId, dr.Name)
+}
+
+func DeleteOutputToResponse(do *deletemodel.DeleteProjectOutput) *deletedto.DeleteResponse {
+	return &deletedto.DeleteResponse{IsDeleted: do.IsDeleted}
 }
