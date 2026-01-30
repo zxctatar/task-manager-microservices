fb8e1f1deda8700c61ce30d56f47611fd617178c
ashlyaud <maksimknutov.w80@gmail.com>
2026-01-31 00:58:31 +0300

    feat: added delete handl implementation

--- projectservice/internal/transport/rest/handler/resthandler.go
+++ projectservice/internal/transport/rest/handler/resthandler.go
@@ -6,9 +6,11 @@ import (
 	"net/http"
 	projectdomain "projectservice/internal/domain/project"
 	createdto "projectservice/internal/transport/rest/handler/dto/create"
+	deletedto "projectservice/internal/transport/rest/handler/dto/delete"
 	handlmapper "projectservice/internal/transport/rest/handler/mapper"
 	handlvalidator "projectservice/internal/transport/rest/handler/validator"
 	createerr "projectservice/internal/usecase/error/createproject"
+	deleteerr "projectservice/internal/usecase/error/deleteproject"
 	"projectservice/internal/usecase/interfaces"
 
 	"github.com/gin-gonic/gin"
@@ -18,12 +20,14 @@ type RestHandler struct {
 	log *slog.Logger
 
 	createProjUC interfaces.CreateProjectUsecase
+	deleteProjUC interfaces.DeleteProjectUsecase
 }
 
-func NewHandler(log *slog.Logger, createProjUC interfaces.CreateProjectUsecase) *RestHandler {
+func NewHandler(log *slog.Logger, createProjUC interfaces.CreateProjectUsecase, deleteProjUC interfaces.DeleteProjectUsecase) *RestHandler {
 	return &RestHandler{
 		log:          log,
 		createProjUC: createProjUC,
+		deleteProjUC: deleteProjUC,
 	}
 }
 
@@ -34,6 +38,7 @@ func (h *RestHandler) Create(ctx *gin.Context) {
 	if val, exists := ctx.Get("userId"); exists {
 		userId = val.(uint32)
 	} else {
+		h.log.Error("failed to get userId")
 		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"error": "internal server error",
 		})
@@ -94,7 +99,69 @@ func (h *RestHandler) Create(ctx *gin.Context) {
 }
 
 func (h *RestHandler) Delete(ctx *gin.Context) {
-	panic("not implemented")
+	const op = "resthandler.Delete"
+
+	var userId uint32
+	if val, exists := ctx.Get("userId"); exists {
+		userId = val.(uint32)
+	} else {
+		h.log.Error("failed to get userId")
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": "internal server error",
+		})
+	}
+
+	log := h.log.With(slog.String("op", op), slog.Int("userId", int(userId)))
+
+	log.Info("start delete request")
+
+	var req *deletedto.DeleteRequest
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		log.Warn("error with request data", slog.String("error", err.Error()))
+		if errMap, ok := handlvalidator.MapValidationErrors(err); ok {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"errors": errMap,
+			})
+		} else {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": "bad request body",
+			})
+		}
+		return
+	}
+
+	in := handlmapper.DeleteRequestToInput(req, userId)
+
+	out, err := h.deleteProjUC.Execute(ctx, in)
+	if err != nil {
+		if errors.Is(err, projectdomain.ErrInvalidName) {
+			log.Info("invalid name")
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+		} else if errors.Is(err, projectdomain.ErrInvalidOwnerId) {
+			log.Info("invalid owner id")
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+		} else if errors.Is(err, deleteerr.ErrProjectNotFound) {
+			log.Info("project not found")
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"error": err.Error(),
+			})
+		} else {
+			log.Warn("cannot delete project", slog.String("error", err.Error()))
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": "internal server error",
+			})
+		}
+	}
+
+	log.Info("delete request completed successfully")
+
+	res := handlmapper.DeleteOutputToResponse(out)
+	ctx.JSON(http.StatusOK, res)
 }
 
 func (h *RestHandler) GetAll(ctx *gin.Context) {
