03d06f44892f70065a2ac1e48f851d2aa79c6e2b
ashlyaud <maksimknutov.w80@gmail.com>
2026-01-31 08:19:17 +0300

    chore: added method get all

--- projectservice/internal/repository/storage/storage.go
+++ projectservice/internal/repository/storage/storage.go
@@ -8,4 +8,5 @@ import (
 type Storage interface {
 	Save(ctx context.Context, proj *projectdomain.ProjectDomain) error
 	Delete(ctx context.Context, proj *projectdomain.ProjectDomain) error
+	GetAll(ctx context.Context, ownerId uint32) ([]*projectdomain.ProjectDomain, error)
 }
