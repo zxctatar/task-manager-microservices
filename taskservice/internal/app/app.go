93d191c13b93d2a0c68a08450abd3169fbeb554f
ashlyaud <maksimknutov90@gmail.com>
2026-02-04 20:55:48 +0300

    chore: added creation user service client and handler

--- taskservice/internal/app/app.go
+++ taskservice/internal/app/app.go
@@ -3,24 +3,31 @@ package app
 import (
 	"context"
 	"taskservice/internal/config"
+	"taskservice/internal/infrastructure/grpc/userservice"
 	"taskservice/internal/transport/rest"
+	resthandler "taskservice/internal/transport/rest/handler"
 	"taskservice/pkg/logger"
 )
 
 type App struct {
 	cfg        *config.Config
 	restServer *rest.RestServer
+	client *userservice.UserServiceClient
 }
 
 func NewApp() *App {
 	cfg := config.MustLoad()
 	log := logger.SetupLogger(cfg.LoggerConf.Level)
 
-	restServer := mustLoadRestServer(cfg, log)
+	client := userservice.NewUserServiceClient(log, cfg.ConnectionsConf.UserServConnConf.Host, cfg.ConnectionsConf.UserServConnConf.Port)
+	handl := resthandler.NewRestHandler(log)
+
+	restServer := mustLoadRestServer(cfg, log, handl, client)
 
 	return &App{
 		cfg:        cfg,
 		restServer: restServer,
+		client: client,
 	}
 }
 
@@ -33,4 +40,5 @@ func (a *App) Stop() {
 	defer cancel()
 
 	a.restServer.Stop(ctx)
+	a.client.Stop()
 }
