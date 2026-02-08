e875522645d549b033785f5567541942b8144dd4
ashlyaud <maksimknutov90@gmail.com>
2026-02-08 04:13:51 +0300

    refactor: updated delete and save queries

--- projectservice/internal/infrastructure/postgres/queries.go
+++ projectservice/internal/infrastructure/postgres/queries.go
@@ -1,7 +1,7 @@
 package postgres
 
 var (
-	QuerieSave   = "INSERT INTO projects(owner_id, name) VALUES($1, $2)"
-	QuerieDelete = "DELETE FROM projects WHERE owner_id = $1 AND name = $2"
+	QuerieSave   = "INSERT INTO projects(owner_id, name) VALUES($1, $2) RETURNING id"
+	QuerieDelete = "DELETE FROM projects WHERE id = $1"
 	QuerieGetAll = "SELECT id, owner_id, name, created_at FROM projects WHERE owner_id = $1"
 )
