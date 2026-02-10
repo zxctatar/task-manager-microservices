b5c064b0db98774acc20a6b89f115166ba65fb38
ashlyaud <maksimknutov.w80@gmail.com>
2026-02-10 11:05:26 +0300

    chore: added create postgres and create task usecase

--- taskservice/internal/app/app.go
+++ taskservice/internal/app/app.go
@@ -2,10 +2,13 @@ package app
 
 import (
 	"context"
+	"database/sql"
 	"taskservice/internal/config"
 	"taskservice/internal/infrastructure/grpc/userservice"
+	"taskservice/internal/infrastructure/postgres"
 	"taskservice/internal/transport/rest"
 	resthandler "taskservice/internal/transport/rest/handler"
+	createuc "taskservice/internal/usecase/implementations/createtask"
 	"taskservice/pkg/logger"
 )
 
@@ -13,14 +16,21 @@ type App struct {
 	cfg        *config.Config
 	restServer *rest.RestServer
 	client     *userservice.UserServiceClient
+	db         *sql.DB
 }
 
 func NewApp() *App {
 	cfg := config.MustLoad()
 	log := logger.SetupLogger(cfg.LoggerConf.Level)
 
+	db := mustLoadPostgres(cfg)
+
+	postgres := postgres.NewPostgres(db)
+
+	createUC := createuc.NewCreateTaskUC(log, postgres)
+
 	client := userservice.NewUserServiceClient(log, cfg.ConnectionsConf.UserServConnConf.Host, cfg.ConnectionsConf.UserServConnConf.Port)
-	handl := resthandler.NewRestHandler(log)
+	handl := resthandler.NewRestHandler(log, createUC)
 
 	restServer := mustLoadRestServer(cfg, log, handl, client)
 
@@ -28,6 +38,7 @@ func NewApp() *App {
 		cfg:        cfg,
 		restServer: restServer,
 		client:     client,
+		db:         db,
 	}
 }
 
@@ -41,4 +52,5 @@ func (a *App) Stop() {
 
 	a.restServer.Stop(ctx)
 	a.client.Stop()
+	a.db.Close()
 }
